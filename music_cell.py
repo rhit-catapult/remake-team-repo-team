5430863d139ddd543f5d8dc1c1342c6cc58cd8fc
rhit-McKinleyFisher <fisher.mckinley@gmail.com>
2026-06-13 11:38:46 -0400

    making 8x16 grid

--- music_cell.py
+++ music_cell.py
@@ -9,8 +9,8 @@ class Cell:
         self.screen = screen
         self.x = 0
         self.y = 0
-        self.width = 100
-        self.height = 100
+        # self.width = 81
+        # self.height = 85
         self.color = color
         self.cells_list = []
         
@@ -37,36 +37,48 @@ class Cell:
 
     def draw(self):
 
-        blocks_horizontal, spacing_horizontal = divmod(self.screen.get_width(), self.width)
-        if spacing_horizontal == 0:
-            spacing_horizontal = self.width
-            blocks_horizontal -= 1
+        self.width, spacing_horizontal = divmod(self.screen.get_width(), 16)
+        
+        # if spacing_horizontal == 0:
+        self.width = 80
+        spacing_horizontal = 20
+
+            
         # self.x = spacing_horizontal / (blocks_horizontal + 1)
 
-        blocks_vertical, spacing_vertical = divmod(self.screen.get_height() - 200, self.height)
-        if spacing_vertical == 0:
-            spacing_vertical = self.height
-            blocks_vertical -= 1
+        self.height, spacing_vertical = divmod(self.screen.get_height() - 180, 8)
+        self.height = 60
+        spacing_vertical = 20
+        # if spacing_vertical == 0:
+        #     spacing_vertical = self.height
+        #     blocks_vertical -= 1
         # self.y = spacing_vertical / (blocks_vertical + 1)
-        self.x =0
-        self.y =0
+        # print(self.width, " x ", self.height)
+        self.x = 0
+        self.y = 0
 
-        print(blocks_vertical, " x ", blocks_horizontal)
-        for k in range(blocks_vertical):
-            for k in range(blocks_horizontal):
+       
+        for k in range(8):
+            for k in range(16):
                 uno_cell = pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
                 self.cells_list.append(uno_cell)
-                self.x += self.width + (spacing_horizontal / (blocks_horizontal - 1))
+                self.x += self.width + (spacing_horizontal / 15)
             self.x = 0
-            self.y += self.height + (spacing_vertical / (blocks_vertical - 1))
-        self.y = 0
+            self.y += self.height + (spacing_vertical / 7)
         
+        self.y += 2
+        for k in range(16):
+            pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, 100))
+            self.x += self.width + (spacing_horizontal / 15)
+
+    
+        pygame.draw.rect(self.screen, pygame.Color("Gray"), (0, 610, self.screen.get_width(), 70))
 
 def main():
     pygame.init()
     pygame.display.set_caption("cells")
     screen = pygame.display.set_mode((1300, 680))
-    cell_grid = Cell(screen, 100, 100, (200, 50, 20))
+    cell_grid = Cell(screen, 100, 100, pygame.Color("White"))
     clock = pygame.time.Clock()
 
     while True:
@@ -75,7 +87,7 @@ def main():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill((255,255,255))
+        screen.fill(pygame.Color("Black"))
         cell_grid.draw()
     
 
