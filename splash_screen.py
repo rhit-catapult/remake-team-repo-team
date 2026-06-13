9dddbfbb340289f55d086ca9f42e71109dec5641
WiseDirt <rylank1016@gmail.com>
2026-06-13 10:32:32 -0400

    Create splash_screen.py

---
+++ splash_screen.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+
+class SplashScreen:
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = pygame.image.load("") # add image path of splash screen here (need to be the same aspect as the screen ratio)
+        self.x = 0
+        self.y = 0
+
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
