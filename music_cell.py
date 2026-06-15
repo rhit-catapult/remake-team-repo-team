9f924658991381fc0b3d93b87ed722d09beb62d2
rhit-McKinleyFisher <fisher.mckinley@gmail.com>
2026-06-15 11:53:06 -0400

    grid configured for True/False statments

--- music_cell.py
+++ music_cell.py
@@ -1,5 +1,4 @@
 import sys
-import random
 import time
 import pygame
 
@@ -8,9 +7,7 @@ class Cell:
     def __init__(self, screen: pygame.Surface, color):
         self.screen = screen
         self.color = color
-        
-            
-
+          
     def draw(self):
         self.x = 0
         self.y = 0
@@ -21,23 +18,34 @@ class Cell:
        
         for k in range(8):
             for k in range(16):
+                # if clicked then:
                 pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
+                # if not clicked then:
+                #pygame.draw.rect(self.screen, pygame.Color("White"), (self.x, self.y, self.width, self.height))
                 self.x += self.width + (spacing_horizontal / 15)
             self.x = 0
             self.y += self.height + (spacing_vertical / 7)
+            #move one data row down
+            #change self.color as rows increase
         
         self.y = 505
+        #self.color = pygame.Color("Blue")
         
         for k in range(16):
-            pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, 100))
+            pygame.draw.rect(self.screen, pygame.Color("White"), (self.x, self.y, self.width, 100))
+            # if not clicked
             pygame.draw.circle(self.screen, pygame.Color("Gray"), (40 + self.x, 535), 8)
+            #else:
+                #pygame.draw.circle(self.screen, self.color, (40 + self.x, 535), 8)
+            #if not clicked:
             pygame.draw.polygon(self.screen, pygame.Color("Gray"), [(40 + self.x, 568), (32 + self.x, 583), (48 + self.x, 583)], 0)
+            #else:
+                #pygame.draw.polygon(self.screen, self.color, [(40 + self.x, 568), (32 + self.x, 583), (48 + self.x, 583)], 0)
+            
             self.x += self.width + (spacing_horizontal / 15)
     
         pygame.draw.rect(self.screen, pygame.Color("Gray"), (0, 610, self.screen.get_width(), 70))
         
-    
-
 
 def main():
     pygame.init()
@@ -59,4 +67,3 @@ def main():
         pygame.display.update()
 
 main()
-
