997fcdb689b43515f5994014a0021c929e446591
WiseDirt <rylank1016@gmail.com>
2026-06-16 10:16:59 -0400

    tempo slider

--- tempo_slider.py
+++ tempo_slider.py
@@ -1,9 +1,19 @@
 import pygame
 import sys
+import data
 
 class Slider:
-    def __init__(self, x, y, width, min_value, max_value, initial_value):
-        self.rect = pygame.Rect(x, y, width, 10)
+    def __init__(self, screen, width, min_value, max_value):
+        self.screen = screen
+        self.color = (50,50,50)
         self.min_value = min_value
         self.max_value = max_value
+        self.inital_value = 60
+        self.width = width
+        self.height = 10
+        self.y = screen.get_height() - self.height - 20
+        self.x = screen.get_width() // 2 - self.width // 2
         
+    def draw(self):
+        pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
+
