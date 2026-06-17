cc2b1da4871a5bd7256a2d1a10e3d7134ebeb260
WiseDirt <rylank1016@gmail.com>
2026-06-17 15:28:08 -0400

    tempo change

--- project.py
+++ project.py
@@ -34,7 +34,7 @@ def main():
     drums_cells = music_cell.Cell(screen, my_data)
     play_button = image_play_button.Button(screen)
     instrument = instrument_button.Instrument(screen)
-    tempo_bar = tempo_slider.Slider(screen, 400, 100, 440, initial_value = 170)
+    tempo_bar = tempo_slider.Slider(screen, 400, 100, 340, initial_value = 220)
     volume_slider = volume.VolumeSlider(screen, 300, 0, 100, initial_value = 50)
     dancer1 = Dancer(screen, 262, 610, image_set = "set1")
     dancer2 = Dancer(screen, 970, 610, image_set = "set2")
